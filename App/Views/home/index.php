ba0a87673f8a061a1fdc9fd7f858ebc560af8df5
Leonardo Santander da Silva <leonardo061297@gmail.com>
2018-12-13 18:43:06 -0200

    Ajustes II

--- App/Views/home/index.php
+++ App/Views/home/index.php
@@ -2,11 +2,14 @@
      <div class="starter-template">
             <center><h1>Bem vindo(a)</h1>
             <h2>Sistema Sindicato dos Frentistas de Bauru</h2></center><br>
-            <h3>Resumo:</h3>
+            
             <table width='50%' align ='center'>
             <tr>
-            <td align ='center'><h3>0</h3> Associados Ativos</td>
-            <td align ='center'><h3>0</h3> Usuarios Ativos</td>
+                <td><strong>Resumo:</strong></td>
+            </tr>
+            <tr>
+                <td align ='center'><h3>0</h3> Associados Ativos</td>
+                <td align ='center'><h3>0</h3> Usuarios Ativos</td>
             </tr>
             </table>
     </div>
