0d888647a1f20a888c9773931515222ec2378d28
locrz <lucas.oliveira.timburi@gmail.com>
2019-02-01 08:47:18 -0200

    Detalhes Escritórios (não carrega dados)

--- App/Views/escritorio/consultar.php
+++ App/Views/escritorio/consultar.php
@@ -58,6 +58,14 @@
                     <td align="center"><?php echo $escritorio->Email;?></td>
                     <td align="center">
                         <a href="http://<?php echo APP_HOST;?>/escritorio/alterar/<?php echo $escritorio->ID_Escritorio?>" class="btn btn-info btn-sm">Editar</a>
+                        <a href='#' class="btn btn-secondary btn-sm" id="details-row" data-toggle="modal" data-target="#exampleModalCenter"
+                                   data-nome="<?php echo $escritorio->NM_Escritorio;?>"
+                                   data-cnpj = "<?php echo $escritorio->CNPJ_Escritorio;?>"
+                                   data-endereco=  "<?php echo $escritorio->NM_Rua.", Nº.: ".$escritorio->NO_Endereco." - ".$escritorio->NM_Bairro." - ".$escritorio->CEP?>"
+                                   data-cidade= "<?php echo $escritorio->NM_Cidade." - ". $escritorio->CD_Estado;?>"
+                                   data-id= "<?php echo $escritorio->ID_Escritorio?>"
+                                   data-telefone = "<?php echo $escritorio->Telefone;?>"
+                                   data-email = "<?php echo $escritorio->Email;?>"<font color="white"> Detalhes</font></a>
                         <a class="btn btn-danger btn-sm" id="delete-row" data-toggle="modal" data-placement="bottom"
                            href="#" data-target="#myModal" aria-hidden="true" data-id="<?php echo $escritorio->ID_Escritorio?>"
                            data-nome="<?php echo $escritorio->NM_Escritorio?>">Excluir</a>
@@ -95,12 +103,67 @@
 	</div>
 </form>
 
+<!-- Abrir a Modal que será utilizada como detalhes -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header bg-dark text-white">
+                <h5 class="modal-title" id="exampleModalCenterTitle">Detalhes de <span id="nomeItemTitulo"></span></h5>
+                <button type="button" class="close text-white" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            <form action="http://<?php echo APP_HOST; ?>/escritorio/alterar" method="post">
+            <input type="hidden" class="form-control" name="id" id="idUpdate">
+
+                <h5>Dados Cadastrais</h5><hr>
+                <strong>Nome Escritório: </strong><span id="teste"></span><br>
+                <strong>CNPJ: </strong> <span id="cnpjItem"></span><br><br>
+
+                <h5>Endereço</h5><hr>
+                <strong>Endereço: </strong> <span id="enderecoItem"></span><br>
+                <strong>Cidade: </strong><span id="cidadeItem"></span><br><br>
+
+                <h5>Contato</h5>
+                <strong>Telefone: </strong><span id="telefoneItem"></span><br>
+                <strong>Email: </strong> <span id="emailItem"></span>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-outline-primary btn-sm" data-dismiss="modal">Voltar</button>
+                <button type="submit" id="UpdateItem" class="btn btn-info btn-sm">Editar</button>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
+
 <script>
     $(window).load(function(){
         $(document).ready(function(){
             var id_delete = -1;
             var nome = "";
             var item = document.getElementById("nomeItem");
+
+            //details
+            var id_detail = -1;
+            var nomeItem = "";
+            var cnpj = "";
+            var endereco = "";
+            var cidade = "";
+            var telefone = "";
+            var email = "";
+
+            var rTitulo = document.getElementById("nomeItemTitulo");
+            var rNome = document.getElementById("teste");
+            var rCnpj = document.getElementById("cnpjItem");
+            var rEndereco = document.getElementById("enderecoItem");
+            var rCidade = document.getElementById("cidadeItem");
+
+            var rTelefone = document.getElementById("telefoneItem");
+            var rEmail = document.getElementById("emailItem");
+
             $("a#delete-row").click(function() {
                 id_delete = $(this).attr('data-id');
                 nome = $(this).attr('data-nome');
@@ -111,6 +174,27 @@
 
                 item.innerHTML = "<strong>" + nome +"</strong>";
             });
+        });
+
+        $("a#details-row").click(function() {
+            id_detail = $(this).attr('data-id');
+            nomeItem = $(this).attr('data-nome');
+            cnpj = $(this).attr('data-cnpj');
+            endereco = $(this).attr('data-endereco');
+            cidade = $(this).attr('data-cidade');
+            telefone = $(this).attr('data-telefone');
+            email = $(this).attr('data-email');
+
+            console.log(nomeItem);
+
+            document.getElementById('idUpdate').value = id_detail;
+
+            rNome.innerHTML = nomeItem;
+            rCnpj.innerHTML = cnpj;
+            rEndereco.innerHTML = endereco;
+            rCidade.innerHTML = cidade;
+            rTelefone.innerHTML = telefone;
+            rEmail.innerHTML = email;
         });
     });
 </script>
