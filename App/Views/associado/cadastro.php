a8ea44d153707ad807215f30f9eafcba27217a65
Leonardo Santander da Silva <leonardo061297@gmail.com>
2019-02-03 01:15:35 -0200

    Numero para Número

--- App/Views/associado/cadastro.php
+++ App/Views/associado/cadastro.php
@@ -315,7 +315,7 @@
                             title="Use somente letras. Não use caracteres especiais ou números." required autofocus>
                     </div>
                     <div class="form-group col-md-2">
-                        <label for="numero">Numero:</label>
+                        <label for="numero">Número:</label>
                         <input type="text" class="form-control" maxlength="5" name="numero" placeholder="000" value="<?php echo $Sessao::retornaValorFormulario('numero'); ?>"
                             pattern="[0-9]+$" onkeydown="javascript: fMasc( this, mNum );" required autofocus>
                     </div>
